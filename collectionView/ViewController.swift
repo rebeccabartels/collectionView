b6dc69071dc7f8e23e671829cf7859e59ad861eb
rebeccabartels <rebeccakbartels@icloud.com>
2016-10-03 23:34:46 -0400

    successfully showed up collection view with 3 elements

---
+++ collectionView/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  collectionView
+//
+//  Created by Rebecca Bartels on 10/3/16.
+//  Copyright © 2016 Rebecca Bartels. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UICollectionViewController {
+
+    @IBOutlet var collectionViewVar: UICollectionView!
+    
+    @IBOutlet weak var collectionViewCellVar: UICollectionViewCell!
+    
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        var cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+
+
+}
+
